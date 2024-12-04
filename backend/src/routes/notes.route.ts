968cef5ce24e964fce3a3f85bbc0d6a8badd504c
Rajahmuden Dalaten <drajahmuden@gmail.com>
2024-12-04 19:59:26 +0800

    Note route

---
+++ backend/src/routes/notes.route.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import { getNotes, createNote, updateNote, deleteNote } from '../controllers/notes.controller'
+
+const router = express.Router();
+
+router.get('/', getNotes);
+
+router.post('/', createNote);
+
+router.patch('/:id', updateNote);
+
+router.delete('/:id', deleteNote);
+
+
+export default router;
