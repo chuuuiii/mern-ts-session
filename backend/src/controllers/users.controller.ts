16ca65d503986d0ea72673946cfa8de8eaca4f0a
Rajahmuden Dalaten <drajahmuden@gmail.com>
2024-12-04 12:25:14 +0800

    Sign up and login controller

--- backend/src/controllers/users.controller.ts
+++ backend/src/controllers/users.controller.ts
@@ -4,6 +4,7 @@ import validator from "validator";
 import bcrypt from 'bcrypt';
 
 
+
 interface SignupBody {
   username: string,
   email: string,
@@ -16,7 +17,7 @@ export const signUp: RequestHandler<unknown, unknown, SignupBody, unknown> = asy
   try {
     //Check for missing fields
     if (!username || !email || !password) {
-       res.status(400).json({ success: false, message: 'Please provide all the details' })
+      res.status(400).json({ success: false, message: 'Please provide all the details' })
     }
     //Validate email
     if (!validator.isEmail(email)) {
@@ -38,6 +39,9 @@ export const signUp: RequestHandler<unknown, unknown, SignupBody, unknown> = asy
     // Create the new user
     const newUser = await User.create({ username, email, password: hash });
 
+    //session
+    // req.session.userId = newUser._id;
+
     // Respond with success
       res.status(201).json({ success: true, data: { id: newUser._id, username: newUser.username, email: newUser.email } })
 
@@ -46,3 +50,41 @@ export const signUp: RequestHandler<unknown, unknown, SignupBody, unknown> = asy
     next(error)
   }
 };
+
+
+interface LoginBody {
+  username: string,
+  password: string,
+}
+
+
+export const login: RequestHandler<unknown, unknown, LoginBody, unknown> = async (req, res, next) => {
+  const { username, password } = req.body;
+
+  try {
+    if (!username || !password) {
+       res.status(400).json({ success: false, message: 'Please provide username and password' });
+    }
+
+    const user = await User.findOne({ username }).select('+password');
+
+    if (!user) {
+       res.status(401).json({ success: false, message: 'Invalid credentials' });
+    }
+
+
+    const passwordMatch = await bcrypt.compare(password, user?.password as string); 
+
+    if (!passwordMatch) {
+      res.status(401).json({ success: false, message: 'Invalid credentials' });
+    }
+
+    
+      res.status(200).json({ success: true, data: user });
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+
